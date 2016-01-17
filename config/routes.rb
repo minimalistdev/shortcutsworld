905fc089b6efc92e1c492cd8d77e409eafb191d9
Christian Franco <cfs.bsi@outlook.com>
2016-01-17 18:29:28 -0200

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # get 'page/index'
-
   root 'page#index'
 
+  # get 'page/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
