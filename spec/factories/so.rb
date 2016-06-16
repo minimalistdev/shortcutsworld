42f135ad3b237d01907a8e1bec2851c28cf0a5b7
christian <christian@christians-MacBook-Pro.local>
2016-06-15 23:44:03 -0300

    remove comments

--- spec/factories/so.rb
+++ spec/factories/so.rb
@@ -1,9 +1,7 @@
 FactoryGirl.define do
 	factory :so do
 		name "Ubuntu"
-		# img "./app/assets/images/profile.png"
 		img do
-    	# you'll get an error if the file doesn't exist
     	File.open(File.join(Rails.root, 'app', 'assets', 'images', 'profile.png'))
 		end
   end
