445adb63a24447d2b818be66c81d33d3096ee8d4
christian <christian@christians-MacBook-Pro.local>
2016-08-13 18:11:18 -0300

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :deploy_to, '/home/deploy/shortcutsworld'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for keep_releases is 5
