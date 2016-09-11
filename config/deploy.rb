8d0c75ee2626961bd0b3ca6481533094b1dc5365
christian <christian@christians-MacBook-Pro.local>
2016-09-11 10:35:28 -0300

    Testing cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,17 +39,15 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-
 namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      within release_path do
-        run "sudo touch #{File.join(current_path,'tmp','restart.txt')}"
-      end
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
     end
   end
 
 end
-
